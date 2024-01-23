e8c767019fa2d2120f1ca64467d58b2b1d15ef53
zhangir-azerbayev <zazerbayev@gmail.com>
2024-01-23 02:14:37 +0000

    example environment configuration

---
+++ scripts/env.sh
@@ -0,0 +1,8 @@
+source /admin/home-zhangir.azerbayev/miniconda3/bin/activate llemma-f2f   
+
+export CUDA_HOME=/admin/home-zhangir.azerbayev/miniconda3/envs/llemma-f2f
+export PATH=/admin/home-zhangir.azerbayev/miniconda3/envs/llemma-f2f/bin:$PATH
+export LD_LIBRARY_PATH=/admin/home-zhangir.azerbayev/miniconda3/envs/llemma-f2f/lib
+
+export HF_HOME=/weka/proj-llemmaf/.cache/huggingface
+export CACHE_DIR=/weka/proj-llemmaf/.cache/lean_dojo
